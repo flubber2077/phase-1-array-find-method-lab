daf91aeb37c8747f8aa98989390f515134e09972
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-04-05 11:24:30 -0400

    commit tree db8fbe9ae7b0c7fa2c4e80fd63a174099eafe3dc

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+let winYear = record => record.result === "W"
+
+let superbowlWin = (record) => {
+    const result = record.find(winYear)
+    return !!result ? result.year : undefined;
+}
